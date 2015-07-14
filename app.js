722c54e4cb0527a03eb73ec3c52542f9123f7a1a
Matt Lavallee <matthew.t.lavallee@gmail.com>
2015-07-13 22:07:53 -0400

    refactor: updating venmo link

--- app.js
+++ app.js
@@ -3,7 +3,7 @@
     var express = require('express');
     var path = require('path');
     var passport = require('passport');
-    //var favicon = require('serve-favicon');
+    var favicon = require('serve-favicon');
     var logger = require('morgan');
     var cookieParser = require('cookie-parser');
     var bodyParser = require('body-parser');
