4e3b282ad4b1a0448193f5fa9595103b6b316813
Matt Lavallee <matthew.t.lavallee@gmail.com>
2016-01-24 13:05:30 -0500

    chore(tests): reducing dup in unit tests

--- tests/private/taproom.service.spec.js
+++ tests/private/taproom.service.spec.js
@@ -12,29 +12,28 @@ describe('Service: TapRoomService', function() {
         timeout = $timeout;
     }));
 
-    describe('getUserEntries function - ', function() {
-        afterEach(function() {
-            httpBackend.verifyNoOutstandingExpectation();
-            httpBackend.verifyNoOutstandingRequest();
-        });
+    afterEach(function() {
+        httpBackend.verifyNoOutstandingExpectation();
+        httpBackend.verifyNoOutstandingRequest();
+    });
 
+    function asyncServiceCallValidation(fnName, params, expectedValue) {
+        factory[fnName](params).then(function(result) {
+            expect(result).toEqual(expectedValue);
+        });
+        httpBackend.flush();
+        timeout.flush();
+    }
+
+    describe('getUserEntries function - ', function() {
         it('Returns a 500 error', function() {
             httpBackend.whenGET('/taproom/user').respond(500, '');
-            factory.getUserEntries().then(function(result) {
-                expect(result).toEqual([]);
-            });
-            httpBackend.flush();
-            timeout.flush();
+            asyncServiceCallValidation('getUserEntries', [], []);
         });
 
         it('Returns null from the api', function() {
             httpBackend.whenGET('/taproom/user').respond(200, null);
-
-            factory.getUserEntries().then(function(result) {
-                expect(result).toEqual([]);
-            });
-            httpBackend.flush();
-            timeout.flush();
+            asyncServiceCallValidation('getUserEntries', [], []);
         });
 
         it('Returns an invalid response from the api', function() {
@@ -42,57 +41,32 @@ describe('Service: TapRoomService', function() {
                 error: true,
                 message: ''
             });
-
-            factory.getUserEntries().then(function(result) {
-                expect(result).toEqual([]);
-            });
-            httpBackend.flush();
-            timeout.flush();
+            asyncServiceCallValidation('getUserEntries', [], []);
         });
 
         it('returns a successful response', function() {
             httpBackend.whenGET('/taproom/user').respond(200, {
                 data: [{}, {}]
             });
-
-            factory.getUserEntries().then(function(result) {
-                expect(result).toBeDefined();
-                expect(result.length).toEqual(2);
-            });
-            httpBackend.flush();
-            timeout.flush();
+            asyncServiceCallValidation('getUserEntries', [], [{}, {}]);
         });
     });
 
     describe('create function - ', function() {
-        afterEach(function() {
-            httpBackend.verifyNoOutstandingExpectation();
-            httpBackend.verifyNoOutstandingRequest();
-        });
-
         it('Returns a 500 error', function() {
             httpBackend.whenPOST('/taproom/new').respond(500, '');
-            factory.create().then(function(result) {
-                expect(result).toEqual({
-                    error: true
-                });
+            asyncServiceCallValidation('create', [], {
+                error: true
             });
-            httpBackend.flush();
-            timeout.flush();
         });
 
         it('Returns null from the api', function() {
             httpBackend.whenPOST('/taproom/new').respond(200, null);
-
-            factory.create().then(function(result) {
-                expect(result).toEqual({
-                    error: true,
-                    id: -1,
-                    message: 'Error creating taproom entry'
-                });
+            asyncServiceCallValidation('create', [], {
+                error: true,
+                id: -1,
+                message: 'Error creating taproom entry'
             });
-            httpBackend.flush();
-            timeout.flush();
         });
 
         it('Returns an invalid response from the api', function() {
@@ -100,63 +74,39 @@ describe('Service: TapRoomService', function() {
                 error: true,
                 message: ''
             });
-
-            factory.create().then(function(result) {
-                expect(result).toEqual({
-                    error: true,
-                    id: -1,
-                    message: 'Error creating taproom entry'
-                });
+            asyncServiceCallValidation('create', [], {
+                error: true,
+                id: -1,
+                message: 'Error creating taproom entry'
             });
-            httpBackend.flush();
-            timeout.flush();
         });
 
         it('Returns a succesful response', function() {
             httpBackend.whenPOST('/taproom/new').respond(200, {
                 id: 1
             });
-            factory.create().then(function(result) {
-                expect(result).toBeDefined();
-                expect(result).toEqual({
-                    error: false,
-                    id: 1
-                });
+            asyncServiceCallValidation('create', [], {
+                error: false,
+                id: 1
             });
-            httpBackend.flush();
-            timeout.flush();
         });
     });
 
     describe('kicked tap function - ', function() {
-        afterEach(function() {
-            httpBackend.verifyNoOutstandingExpectation();
-            httpBackend.verifyNoOutstandingRequest();
-        });
-
         it('Returns a 500 error', function() {
             httpBackend.whenPOST('/taproom/kickTap').respond(500, '');
-            factory.kickTap(1).then(function(result) {
-                expect(result).toEqual({
-                    error: true
-                });
+            asyncServiceCallValidation('kickTap', [1], {
+                error: true
             });
-            httpBackend.flush();
-            timeout.flush();
         });
 
         it('Returns null from the api', function() {
             httpBackend.whenPOST('/taproom/kickTap').respond(200, null);
-
-            factory.kickTap(1).then(function(result) {
-                expect(result).toEqual({
-                    error: true,
-                    id: -1,
-                    message: 'Error kicking taproom entry'
-                });
+            asyncServiceCallValidation('kickTap', [1], {
+                error: true,
+                id: -1,
+                message: 'Error kicking taproom entry'
             });
-            httpBackend.flush();
-            timeout.flush();
         });
 
         it('Returns an invalid response from the api', function() {
@@ -164,63 +114,39 @@ describe('Service: TapRoomService', function() {
                 error: true,
                 message: ''
             });
-
-            factory.kickTap(1).then(function(result) {
-                expect(result).toEqual({
-                    error: true,
-                    id: -1,
-                    message: 'Error kicking taproom entry'
-                });
+            asyncServiceCallValidation('kickTap', [1], {
+                error: true,
+                id: -1,
+                message: 'Error kicking taproom entry'
             });
-            httpBackend.flush();
-            timeout.flush();
         });
 
         it('Returns a succesful response', function() {
             httpBackend.whenPOST('/taproom/kickTap').respond(200, {
                 id: 1
             });
-            factory.kickTap(1).then(function(result) {
-                expect(result).toBeDefined();
-                expect(result).toEqual({
-                    error: false,
-                    id: 1
-                });
+            asyncServiceCallValidation('kickTap', [1], {
+                error: false,
+                id: 1
             });
-            httpBackend.flush();
-            timeout.flush();
         });
     });
 
     describe('delete tap function - ', function() {
-        afterEach(function() {
-            httpBackend.verifyNoOutstandingExpectation();
-            httpBackend.verifyNoOutstandingRequest();
-        });
-
         it('Returns a 500 error', function() {
             httpBackend.whenPOST('/taproom/deleteTap').respond(500, '');
-            factory.deleteTap(1).then(function(result) {
-                expect(result).toEqual({
-                    error: true
-                });
+            asyncServiceCallValidation('deleteTap', [1], {
+                error: true
             });
-            httpBackend.flush();
-            timeout.flush();
         });
 
         it('Returns null from the api', function() {
             httpBackend.whenPOST('/taproom/deleteTap').respond(200, null);
-
-            factory.deleteTap(1).then(function(result) {
-                expect(result).toEqual({
-                    error: true,
-                    id: -1,
-                    message: 'Error deleting taproom entry'
-                });
+            asyncServiceCallValidation('deleteTap', [1], {
+                error: true,
+                id: -1,
+                message: 'Error deleting taproom entry'
             });
-            httpBackend.flush();
-            timeout.flush();
         });
 
         it('Returns an invalid response from the api', function() {
@@ -228,63 +154,39 @@ describe('Service: TapRoomService', function() {
                 error: true,
                 message: ''
             });
-
-            factory.deleteTap(1).then(function(result) {
-                expect(result).toEqual({
-                    error: true,
-                    id: -1,
-                    message: 'Error deleting taproom entry'
-                });
+            asyncServiceCallValidation('deleteTap', [1], {
+                error: true,
+                id: -1,
+                message: 'Error deleting taproom entry'
             });
-            httpBackend.flush();
-            timeout.flush();
         });
 
         it('Returns a succesful response', function() {
             httpBackend.whenPOST('/taproom/deleteTap').respond(200, {
                 id: 1
             });
-            factory.deleteTap(1).then(function(result) {
-                expect(result).toBeDefined();
-                expect(result).toEqual({
-                    error: false,
-                    id: 1
-                });
+            asyncServiceCallValidation('deleteTap', [1], {
+                error: false,
+                id: 1
             });
-            httpBackend.flush();
-            timeout.flush();
         });
     });
 
     describe('pour drink function - ', function() {
-        afterEach(function() {
-            httpBackend.verifyNoOutstandingExpectation();
-            httpBackend.verifyNoOutstandingRequest();
-        });
-
         it('Returns a 500 error', function() {
             httpBackend.whenPOST('/taproom/pourDrink').respond(500, '');
-            factory.pourDrink(1, 2).then(function(result) {
-                expect(result).toEqual({
-                    error: true
-                });
+            asyncServiceCallValidation('pourDrink', [1, 2], {
+                error: true
             });
-            httpBackend.flush();
-            timeout.flush();
         });
 
         it('Returns null from the api', function() {
             httpBackend.whenPOST('/taproom/pourDrink').respond(200, null);
-
-            factory.pourDrink(1, 2).then(function(result) {
-                expect(result).toEqual({
-                    error: true,
-                    id: -1,
-                    message: 'Error pouring taproom entry'
-                });
+            asyncServiceCallValidation('pourDrink', [1, 2], {
+                error: true,
+                id: -1,
+                message: 'Error pouring taproom entry'
             });
-            httpBackend.flush();
-            timeout.flush();
         });
 
         it('Returns an invalid response from the api', function() {
@@ -292,31 +194,21 @@ describe('Service: TapRoomService', function() {
                 error: true,
                 message: ''
             });
-
-            factory.pourDrink(1, 2).then(function(result) {
-                expect(result).toEqual({
-                    error: true,
-                    id: -1,
-                    message: 'Error pouring taproom entry'
-                });
+            asyncServiceCallValidation('pourDrink', [1, 2], {
+                error: true,
+                id: -1,
+                message: 'Error pouring taproom entry'
             });
-            httpBackend.flush();
-            timeout.flush();
         });
 
         it('Returns a succesful response', function() {
             httpBackend.whenPOST('/taproom/pourDrink').respond(200, {
                 id: 1
             });
-            factory.pourDrink(1, 2).then(function(result) {
-                expect(result).toBeDefined();
-                expect(result).toEqual({
-                    error: false,
-                    id: 1
-                });
+            asyncServiceCallValidation('pourDrink', [1, 2], {
+                error: false,
+                id: 1
             });
-            httpBackend.flush();
-            timeout.flush();
         });
     });
 });
