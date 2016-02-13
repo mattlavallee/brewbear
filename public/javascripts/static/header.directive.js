a2aff4b4baf7d001a6da4df49cbb8a9b2aa8b83a
Matt Lavallee <matthew.t.lavallee@gmail.com>
2016-02-13 14:24:22 -0500

    fix(ui): making the correct nav link be active

--- public/javascripts/static/header.directive.js
+++ public/javascripts/static/header.directive.js
@@ -1,12 +1,41 @@
 (function() {
     'use strict';
 
-    var HeaderDirective = function() {
+    function HeaderDirective($location) {
+        function handlePathUpdate(newPath, parent) {
+            //remove the current active class
+            parent.find('li.active').removeClass('active');
+
+            var newActiveClass;
+            switch (newPath) {
+                case '/taproom':
+                    newActiveClass = '.bb-nav-tap';
+                    break;
+                case '/trends':
+                    newActiveClass = '.bb-nav-trends';
+                    break;
+                case '/account':
+                    newActiveClass = '.bb-nav-profile';
+                    break;
+                default:
+                    newActiveClass = '.bb-nav-bar';
+            }
+
+            parent.find(newActiveClass).addClass('active');
+        }
+
         return {
             templateUrl: '/javascripts/static/header.template.html',
-            restrict: 'E'
+            restrict: 'E',
+            link: function(scope, element) {
+                scope.$watch(function() {
+                    return $location.path();
+                }, function(newPath) {
+                    handlePathUpdate(newPath, element);
+                });
+            }
         };
-    };
+    }
 
     angular.module('bb-static')
         .directive('brewBearHeader', HeaderDirective);
--- tests/static/header.directive.spec.js
+++ tests/static/header.directive.spec.js
@@ -1,20 +1,55 @@
 describe('Directive: Header', function() {
     'use strict';
 
-    var element, scope;
+    var element, scope, location;
 
     beforeEach(module('bb-static', 'brewbear-templates'));
 
-    beforeEach(inject(function($rootScope, $compile) {
+    beforeEach(inject(function($rootScope, $compile, $location) {
+        location = $location;
         scope = $rootScope.$new();
 
         element = '<brew-bear-header></brew-bear-header>';
         element = $compile(element)(scope);
-        scope.$digest();
+        scope.$apply();
     }));
 
     it('Verifies the directive is instantiated', function() {
         expect(element).toBeDefined();
         expect(element.find('.navbar').length).toEqual(1);
+    });
+
+    describe('path $watch', function() {
+        it('correctly updates the active link to tap', function() {
+            location.path('/taproom');
+            scope.$apply();
+
+            var newActive = element.find('.active');
+            expect(newActive.hasClass('bb-nav-tap')).toEqual(true);
+        });
+
+        it('correctly updates the active link to trends', function() {
+            location.path('/trends');
+            scope.$apply();
+
+            var newActive = element.find('.active');
+            expect(newActive.hasClass('bb-nav-trends')).toEqual(true);
+        });
+
+        it('correctly updates the active link to profile', function() {
+            location.path('/account');
+            scope.$apply();
+
+            var newActive = element.find('.active');
+            expect(newActive.hasClass('bb-nav-profile')).toEqual(true);
+        });
+
+        it('defaults to bar as the active link', function() {
+            location.path('/');
+            scope.$apply();
+
+            var newActive = element.find('.active');
+            expect(newActive.hasClass('bb-nav-bar')).toEqual(true);
+        });
     });
 });
